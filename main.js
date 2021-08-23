1d5564936f8973c2e0faf7d91fcf7bdf01525d76
hatso <mmorpg3636@naver.com>
2021-08-23 21:20:03 +0900

    nodejs work Day 2-2

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring')
 
-function templateHTML(title, list, body){
+function templateHTML(title, list, body, control){
   return `
   <!doctype html>
   <html>
@@ -13,6 +14,7 @@ function templateHTML(title, list, body){
   <body>
     <h1><a href="/">WEB</a></h1>
     ${list}
+    ${control}
     ${body}
   </body>
   </html>
@@ -41,8 +43,11 @@ var app = http.createServer(function(request, response) {
           var title ='Welcome';
           var description = 'Hollw Node.js';
           var list = templateList(filelist);
-          var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
-          
+          var template = templateHTML(title, list,
+            `<h2>${title}</h2>${description}`,
+            `<a href="/create">create</a>`
+          );
+
           response.writeHead(200);
           response.end(template);
         });
@@ -54,16 +59,50 @@ var app = http.createServer(function(request, response) {
 
           fs.readFile(`data/${queryData.id}`,'utf8', function(err,description){
             var title = queryData.id;
-            var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+            var template = templateHTML(title, list,
+              `<h2>${title}</h2>${description}`,
+              `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+            );
             response.writeHead(200);
             response.end(template);
           });
         });
       }
-    } else{
+    } else if(pathname === '/create'){
+      fs.readdir('./data', function(error, filelist){
+        var title ='Web - create';
+        var list = templateList(filelist);
+        var template = templateHTML(title, list, `
+                <form action="http://localhost:3000/create_process" method="post">
+                    <p><input type="text" name="title" placeholder="title"></p>
+                    <p>
+                        <textarea name="description" placeholder="description"></textarea>
+                    </p>
+                    <p>
+                        <input type="submit">
+                    </p>
+                </form>
+            `,'');
+        response.writeHead(200);
+        response.end(template);
+      });
+    } else if(pathname === '/create_process') {
+        var body = '';
+        request.on('data', function(data) {
+            body = body + data;
+        });
+        request.on('end', function() {
+            var post = qs.parse(body);
+            var title = post.title; //post로 받은 data에 접근하여 값을 받아옴.
+            var description = post.description;
+            fs.writeFile(`data/${title}`, description, 'utf8', function(err) {
+                response.writeHead(302, {Location: `/?id=${title}`}); //파일 위치변경 302는 지금 이순간에만 바뀐다는걸 알려줌.
+                response.end();
+            });
+        });
+    } else {
       response.writeHead(404);
       response.end('Not Found');
     }
-
 });
-app.listen(3000);
+app.listen(3000); //port 번호 3000을 listen함.
